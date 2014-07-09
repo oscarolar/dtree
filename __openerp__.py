2e11f157ef457f220f0e01801cd656ed7be8b010
Olar Alca <oszckar@gmail.com>
2014-07-09 12:14:49 -0500

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Dynatree Test",
+    "category": "Hidden",
+    "description":
+        """
+        Provide jquery.dynatree selectors for hierarchical data,
+        such as products categories.
+        """,
+    "version": "0.2",
+    "depends": [
+        'account',
+        'analytic',
+        ],
+    "data":['aaa.xml'],
+    "js": [],
+    "css": [],
+    'qweb': [],
+    'auto_install': True,
+}
